64484eadffecc2644d275ef0df97e0650a7b5582
ricardin <richard.foulu@gmail.com>
2025-01-29 16:33:37 -0300

    Atualizacao

---
+++ GitGithub/1 poweroff.sh
@@ -0,0 +1,95 @@
+    1  poweroff
+    2  sudo
+    3  sudo apt
+    4  sudo apt update
+    5  sudo apt upgrade
+    6  sudo apt upgrade --fix-missing
+    7  sudo apt autoremove
+    8  sudo apt autoclean
+    9  sudo reboot
+   10  curl -fsS https://dl.brave.com/install.sh | sh
+   11  sudo apt update
+   12  sudo apt install snapd~
+   13  sudo snap install midori
+   14  npx @puppeteer/browsers install chrome@stable
+   15  ~sudo apt update
+   16  sudo apt install snapd
+   17  sudo snap install midori
+   18  sudo snap refresh midori
+   19  echo -e '[gentoo-snappy]\n location = /usr/local/portage/gentoo-snappy\n sync-type = git\n sync-uri = https://github.com/zyga/gentoo-snappy.git\n priority = 50\n auto-sync = yes
+   20  \n Categories=Application' | sudo tee /etc/portage/repos.conf/gentoo-snappy.conf
+   21  ~emaint sync --repo gentoo-snappy
+   22  sudo add-apt-repository ppa:xalt7x/chromium-deb-vaapi
+   23  cat <<EOF | sudo tee /etc/apt/preferences.d/pin-xalt7x-chromium-deb-vaapi
+   24  Package: *
+   25  Pin: release o=LP-PPA-xalt7x-chromium-deb-vaapi
+   26  Pin-Priority: 1337
+   27  EOF
+   28  sudo apt-get update
+   29  sudo apt install chromium-browser chromium-codecs-ffmpeg
+   30  e
+   31  E
+   32  sudo add-apt-repository ppa:xalt7x/chromium-deb-vaapi
+   33  sudo add-apt-repository ppa:xalt7x/chromium-deb-vaapi -r -y
+   34  sudo add-apt-repository ppa:a-v-shkop/chromium
+   35  sudo apt-get update && sudo apt-get install chromium-browser
+   36  sudo add-apt-repository ppa:a-v-shkop/chromium-dev
+   37  sudo apt-get update
+   38  sudo apt-get upgrade
+   39  sudo add-apt-repository ppa:a-v-shkop/chromium
+   40  sudo apt install ./*.deb
+   41  ~sudo dpkg -i chromium*.deb
+   42  wget http://packages.linuxmint.com/pool/upstream/c/chromium-browser/chromium-browser_2020.10.27_all.deb
+   43  sudo install chromium
+   44  sudo apt update
+   45  sudo apt upgrade
+   46  sudo apt install chromium
+   47  sudo apt update
+   48  sudo apt install git vim
+   49  sudo apt update
+   50  sudo apt install kolourpaint
+   51  sudo apt update
+   52  sudo apt install kolourpaint
+   53  sudo apt install KolourPaint
+   54  sudo apt upgrade
+   55  sudo apt-get install --reinstall packagename
+   56  sudo apt -reinstall ttf-mscorefonts-installer
+   57  sudo apt install koloupaint
+   58  sudo apt install kolourpaint
+   59  sudo apt update
+   60  sudo apt install kolourpaint
+   61  sudo apt updatte
+   62  sudo apt update
+   63  sudo apt install git vim
+   64  sudo apt update
+   65  sudo apt install kolourpaint
+   66  sudo apt update 
+   67  y
+   68  s
+   69  sudo apt install kolourpaint
+   70  sudo apt update
+   71  sudo apt install kolourpaint
+   72  sudo apt install linux-oem-24.04b
+   73  sudo reboot
+   74  sudo apt install kolourpaint
+   75  sudo apt install vlc
+   76  sudo apt install notepadqq
+   77  sudo apt install audacity
+   78  sudo apt install kazam
+   79  sudo apt install kdenlive
+   80  git
+   81  git config ==global user.name "Ricardin"
+   82  git config --global user.name "ricardin"
+   83  git config --global user.email 
+   84  git 
+   85  git config --global user.name "Ricardin"
+   86  git config --global user.name  " Ricardn "
+   87  git
+   88  git config --global user.name  Ricardn
+   89  sudo apt install git vim
+   90  git config --global user.name ricardin
+   91  git config --global user.name " ricardin "
+   92  git config --global user.email 
+   93  git  clone https://github.com/Ricardiin/senac-tat-t
+   94  exit
+   95  history
